d1f8f60e5097d87f149912d7a5d46601dd7fe448
tanxiaoxiansheng <1148638518@qq.com>
2018-11-26 23:37:08 +0800

    mybatis自动生成项目 SSM03截至到单元测试

---
+++ SSM03/src/main/java/com/javen/service/impl/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package com.javen.service.impl;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.javen.dao.UserDao;
+import com.javen.model.User;
+import com.javen.service.UserService;
+
+@Service("userService")
+public class UserServiceImpl implements UserService{
+	
+	@Resource
+	private UserDao userDao;
+
+	public User getUserById(int userId) {
+		return userDao.selectByPrimaryKey(userId);
+	}
+
+	
+}
